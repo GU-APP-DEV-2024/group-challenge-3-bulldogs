bcf4c5d8ee3cda1f870b14bacd359016b0cbbe65
EthanD <edanitz@zagmail.gonzaga.edu>
2024-10-03 16:26:09 -0700

    Participation from the Board

--- app/src/main/java/memorygame/Board.kt
+++ app/src/main/java/memorygame/Board.kt
@@ -1,14 +1,20 @@
 package memorygame
+import android.util.Log
 
 class Board( ) {
     //Constructor to create board of different sizes.
     // Assign
-    private val cards: Array<Card> = arrayOf<Card>(Card('c', "a"),
-        Card('h', "10"),
-        Card('h', "10"),
-        Card('d', "3"),
-        Card('c', "a"),
-        Card('d', "3"))
+    private val availableCards = arrayOf<Card>(Card('s', "4"), Card('h', "10"), Card('d', "3"))
+    private var cards: Array<Card> = Array<Card>(6) {_ -> Card(' ',"")}
+
+    init {
+        for(i in 0..2) {
+            Log.d("MemoryBoard", "${2*i}, ${(2*i)+1}")
+            cards[2*i] = availableCards.random()
+            cards[(2*i)+1] = cards[2*i]
+        }
+        cards.shuffle()
+    }
 
     fun getValueAt(index:Int): Card {
         return cards[index]
